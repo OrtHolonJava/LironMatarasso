3079d584922b0bebaddf2b30ecfe859dfcd4cae7
lironi1111 <lironi1111@gmail.com>
2017-12-05 22:27:02 +0200

    updated map

--- src/MapPanel.java
+++ src/MapPanel.java
@@ -15,15 +15,17 @@ public class MapPanel extends JPanel {
 	private Map _map;
 	private Img _sandBlock;
 	private Img _stoneBlock;
+	private Img _seaweedBlock;
 
 	public MapPanel() {
 		_mapFile = "MapFiles//GetPicture_20171205155505.xml";
 		_size = Map.getElementCountByName(_mapFile,"Line");
 		_sizeW = Map.getElementCountByName(_mapFile,"Area")/_size;
-		_blockSize = 8;
+		_blockSize = 6;
 		_backgroundImg = new Img("images//Background.jpg", 0, 0, _sizeW * _blockSize, _size * _blockSize);
 		_sandBlock = new Img("images/SandBlock.png", 0, 0, _blockSize, _blockSize);
 		_stoneBlock = new Img("images//StoneBlock.png", 0, 0, _blockSize, _blockSize);
+		_seaweedBlock= new Img("images//OneSW.png", 0, 0, _blockSize, _blockSize);
 		_map = new Map(_size, _sizeW, _mapFile);
 
 	}
@@ -44,6 +46,11 @@ public class MapPanel extends JPanel {
 				case 2: {
 					_stoneBlock.setImgCords((j * _blockSize), (i) * _blockSize);
 					_stoneBlock.drawImg(g);
+					break;
+				}
+				case 3: {
+					_seaweedBlock.setImgCords((j * _blockSize), (i) * _blockSize);
+					_seaweedBlock.drawImg(g);
 					break;
 				}
 				}
