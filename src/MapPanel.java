22d609edeb6198e5e80e43168f753376b5bf4142
lironi1111 <lironi1111@gmail.com>
2017-12-06 11:22:44 +0200

    Added background blocks

--- src/MapPanel.java
+++ src/MapPanel.java
@@ -16,16 +16,20 @@ public class MapPanel extends JPanel {
 	private Img _sandBlock;
 	private Img _stoneBlock;
 	private Img _seaweedBlock;
+	private Img _sandBackground;
+	private Img _stoneBackground;
 
 	public MapPanel() {
-		_mapFile = "MapFiles//GetPicture_20171205155505.xml";
+		_mapFile = "MapFiles//GetPicture_20171206112109.xml";
 		_size = Map.getElementCountByName(_mapFile,"Line");
 		_sizeW = Map.getElementCountByName(_mapFile,"Area")/_size;
-		_blockSize = 6;
+		_blockSize = 7;
 		_backgroundImg = new Img("images//Background.jpg", 0, 0, _sizeW * _blockSize, _size * _blockSize);
-		_sandBlock = new Img("images/SandBlock.png", 0, 0, _blockSize, _blockSize);
-		_stoneBlock = new Img("images//StoneBlock.png", 0, 0, _blockSize, _blockSize);
+		_sandBlock = new Img("images//SandBlock2.png", 0, 0, _blockSize, _blockSize);
+		_stoneBlock = new Img("images//‏‏StoneBlock2.png", 0, 0, _blockSize, _blockSize);
 		_seaweedBlock= new Img("images//OneSW.png", 0, 0, _blockSize, _blockSize);
+		_sandBackground= new Img("images//SandBackground.png", 0, 0, _blockSize, _blockSize);
+		_stoneBackground= new Img("images//StoneBackground.png", 0, 0, _blockSize, _blockSize);
 		_map = new Map(_size, _sizeW, _mapFile);
 
 	}
@@ -51,6 +55,16 @@ public class MapPanel extends JPanel {
 				case 3: {
 					_seaweedBlock.setImgCords((j * _blockSize), (i) * _blockSize);
 					_seaweedBlock.drawImg(g);
+					break;
+				}
+				case 4: {
+					_sandBackground.setImgCords((j * _blockSize), (i) * _blockSize);
+					_sandBackground.drawImg(g);
+					break;
+				}
+				case 5: {
+					_stoneBackground.setImgCords((j * _blockSize), (i) * _blockSize);
+					_stoneBackground.drawImg(g);
 					break;
 				}
 				}
