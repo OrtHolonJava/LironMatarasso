0757082a2bea08d3fd4c3e63f53a664057e26a68
lironi1111 <lironi1111@gmail.com>
2018-01-04 18:23:03 +0200

    added stupid sharks

--- src/MapFrame.java
+++ src/MapFrame.java
@@ -1,13 +1,16 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 public class MapFrame extends JFrame {
 	private MapPanel _mapPanel;
 
 	public MapFrame() {
+		setLayout(new BorderLayout());
 		_mapPanel = new MapPanel();
-		add(_mapPanel);
+		add(_mapPanel,BorderLayout.CENTER);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(2000,1000);
+		setSize(2000, 1000);
 		setVisible(true);
 	}
 }
--- src/MapPanel.java
+++ src/MapPanel.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -43,8 +44,7 @@ public class MapPanel extends JPanel implements ActionListener {
 		_sizeW = Map.getElementCountByName(_mapFile, "Area") / _size;
 		_blockSize = 40;
 		_mousePoint = new Point(0, 0);
-		_centerPoint = new Point(1000, 500);
-		System.out.println("cent: " + _centerPoint.toString());
+		_centerPoint = new Point(0, 0);
 		_camPoint = new Point(_centerPoint);
 
 		_backgroundImg = new Img("images//Background.jpg", 0, 0, _sizeW * _blockSize, _size * _blockSize);
@@ -53,47 +53,10 @@ public class MapPanel extends JPanel implements ActionListener {
 		_seaweedBlock = new Img("images//OneSW.png", 0, 0, _blockSize, _blockSize);
 		_sandBackground = new Img("images//SandBackground.png", 0, 0, _blockSize, _blockSize);
 		_stoneBackground = new Img("images//StoneBackground.png", 0, 0, _blockSize, _blockSize);
-		_shark = new Img("images//shark1.png", 0, 0, 66, 122);
-		System.out.println(_camPoint.x + " " + _camPoint.y);
-		_shark.setImgCords(_camPoint.x, _camPoint.y);
-		img = Img.toBufferedImage(_shark.getImage());
+		_shark = new Img("images//shark1.png", 0, 0, 33, 61);
 		_map = new Map(_size, _sizeW, _mapFile, _effectsFile);
 		Timer t = new Timer(10, this);
 		t.start();
-	}
-
-	protected void rotateShark(Graphics g) {
-
-		// System.out.println(_camPoint.x + " " + _camPoint.y);
-		Graphics2D g2d = (Graphics2D) g.create();
-
-		double rotation = 0f;
-
-		int width = getWidth() - 1;
-		int height = getHeight() - 1;
-
-		if (_camPoint != null) {
-
-			int x = width / 2;
-			int y = height / 2;
-
-			int deltaX = _camPoint.x - x;
-			int deltaY = _camPoint.y - y;
-
-			rotation = -Math.atan2(deltaX, deltaY);
-
-			rotation = Math.toDegrees(rotation) + 180;
-
-		}
-
-		int x = (width - img.getWidth()) / 2;
-		int y = (height - img.getHeight()) / 2;
-
-		g2d.rotate(Math.toRadians(rotation), width / 2, height / 2);
-		g2d.drawImage(img, x, y, this);
-		x = width / 2;
-		y = height / 2;
-		g2d.dispose();
 	}
 
 	protected void rotateShark2(Graphics g) {
@@ -109,8 +72,6 @@ public class MapPanel extends JPanel implements ActionListener {
 
 			int x = width / 2;
 			int y = height / 2;
-			System.out.println("w: " + x + " h: " + y);
-
 			int deltaX = _mousePoint.x - x;
 			int deltaY = _mousePoint.y - y;
 
@@ -122,21 +83,20 @@ public class MapPanel extends JPanel implements ActionListener {
 
 		int x = (width - img.getWidth()) / 2;
 		int y = (height - img.getHeight()) / 2;
-
 		g2d.rotate(Math.toRadians(rotation), width / 2, height / 2);
 		g2d.drawImage(img, x, y, this);
-		x = width / 2;
-		y = height / 2;
+		/*
+		 * x = width / 2; y = height / 2;
+		 */
 		g2d.dispose();
 	}
 
 	@Override
 	protected void paintComponent(Graphics g1) {
 		// TODO Auto-generated method stub
-		getPreferredSize();
 		Graphics2D g = (Graphics2D) g1;
 		super.paintComponent(g);
-		g.translate(-_camPoint.getX(), -_camPoint.getY() / 2);
+		g.translate(-_camPoint.getX(), -_camPoint.getY());
 		_backgroundImg.drawImg(g);
 		for (int i = 0; i < _size; i++) {
 			for (int j = 0; j < _sizeW; j++) {
@@ -169,28 +129,42 @@ public class MapPanel extends JPanel implements ActionListener {
 				}
 			}
 		}
+		_shark.setImgCords(_centerPoint.x, _centerPoint.y);
+		_shark.drawImg(g);
+		img = Img.toBufferedImage(_shark.getImage());
 		rotateShark2(g);
+
+		g.setColor(Color.red);
+		g.drawOval(_mousePoint.x, _mousePoint.y, 100, 100);
+		g.setColor(Color.yellow);
+		g.drawOval(_centerPoint.x, _centerPoint.y, 100, 100);
+		g.setColor(Color.green);
+		g.drawOval(_camPoint.x, _camPoint.y, 100, 100);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		_mousePoint = MouseInfo.getPointerInfo().getLocation();
+		_mousePoint.setLocation(_camPoint.x + _mousePoint.x, _camPoint.y + _mousePoint.y);
+		_centerPoint.setLocation(_camPoint.x + getWidth() / 2, _camPoint.y + getHeight() / 2);
+
 		// System.out.println("mouse: " + _mousePoint.toString());
 		// System.out.println("cam: " + _camPoint.toString());
 		// System.out.println("center: " + _centerPoint.toString());
 
 		if (_centerPoint.getX() < _mousePoint.getX())
-			_camPoint.setLocation(_camPoint.getX() + 1, _mousePoint.getY());
+			_camPoint.setLocation(_camPoint.getX() + 1, _camPoint.getY());
 
 		if (_centerPoint.getX() > _mousePoint.getX())
-			_camPoint.setLocation(_camPoint.getX() - 1, _mousePoint.getY());
+			_camPoint.setLocation(_camPoint.getX() - 1, _camPoint.getY());
 
 		if (_centerPoint.getY() < _mousePoint.getY())
-			_camPoint.setLocation(_camPoint.getX(), _mousePoint.getY() + 1);
+			_camPoint.setLocation(_camPoint.getX(), _camPoint.getY() + 1);
 
 		if (_centerPoint.getY() > _mousePoint.getY())
-			_camPoint.setLocation(_camPoint.getX(), _mousePoint.getY() - 1);
+			_camPoint.setLocation(_camPoint.getX(), _camPoint.getY() - 1);
 		repaint();
 	}
 
